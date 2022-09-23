5321ea8d917cea4ac4c2bc31e46cf21a811575d3
LeonardoLMachado <leonardolmachado@outlook.com.br>
2022-09-23 09:26:12 -0300

    12/09

--- _12Setembro22/maisSobreObjetos.js
+++ _12Setembro22/maisSobreObjetos.js
@@ -18,11 +18,55 @@ mostraContinente(pais);
 
 /* Desestruturação em ARRAYS */
 const alturas = [
-    ['bruno', 183],
-    ['tunico', 187]
-]
+  ["bruno", 183],
+  ["tunico", 187],
+];
 
-const [ nome, altura ] = alturas[0]
-console.log('O ' + nome + ' tem ' + altura + 'cm de altura')
+const [nome, altura] = alturas[0];
+console.log("O " + nome + " tem " + altura + "cm de altura");
 
-/* 01:53:00 */
+// SPREAD OPERATOR
+// Desestruturar objetos ou Arrays em outros objetos ou arrays
+
+const fruta = {
+  nome: "abacate",
+  tipo: "tropical",
+};
+
+const dadosNutricionais = {
+  id: "1234ABCD",
+  calorias: "150Kcal",
+  porcao: "20g",
+  ...fruta /* SPREAD */,
+};
+
+console.log(dadosNutricionais.tipo);
+
+function soma2(numeros) {
+  numeros.forEach((num) => console.log(num + 2));
+}
+
+const array1= [1, 2, 3, 4];
+soma2([...array1, 5, 6, 7]);
+
+// REST OPERATOR
+
+const pessoa = {
+  nome: 'Leo',
+  idade: 32,
+  corPredileta: 'verde'
+}
+
+function entregaPresente (pessoa){
+  const { corPredileta, ...rest } = pessoa
+
+  if (pessoa.corPredileta == 'vermelho') console.log('Parabens! Toma aqui o seu presente!')
+  else {
+    console.log('O presente não é pra você.')
+  }
+
+  return rest
+}
+
+const restoDoObjeto = entregaPresente(pessoa)
+console.log(restoDoObjeto)
