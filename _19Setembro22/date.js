fcb47a1c6ce8b3d18006f24f280f4c3ceac75363
LeonardoLMachado <leonardolmachado@outlook.com.br>
2022-09-27 13:30:05 -0300

    19/09

---
+++ _19Setembro22/date.js
@@ -0,0 +1,7 @@
+
+let agora = new Date();
+console.log(agora);
+
+/* configurando parametros da New Date */
+console.log(agora.toLocaleDateString("pt-BR"))
+console.log(agora.toLocaleString("pt-BR"))
