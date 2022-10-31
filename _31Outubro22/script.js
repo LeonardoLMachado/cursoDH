c4fdb8c05ab5da414d8c32b2895fe0ff1992082f
LeonardoLMachado <leonardolmachado@outlook.com.br>
2022-10-31 18:44:47 -0300

    JS no Front

---
+++ _31Outubro22/script.js
@@ -0,0 +1,46 @@
+window.addEventListener("load", function () {
+  // escreva seu código aqui
+  let tituloFilme = document.querySelector("h2");
+});
+
+window.addEventListener("load", function () {
+  // escreva seu codigo aqui
+  let paragrafos = document.querySelectorAll("p");
+});
+
+window.addEventListener("load", function () {
+  // Escreva seu codigo aqui
+  let pulpFiction = document.getElementById("pulp-fiction");
+});
+
+window.addEventListener("load", function () {
+  // escreva seu codigo aqui
+  let barraDeNavegacao = document.getElementsByClassName("nav-bar");
+});
+
+window.addEventListener("load", function () {
+  // escreva seu código aqui
+  document.querySelector("h3").innerText += " usando código";
+});
+
+window.addEventListener("load", function () {
+  // escreva seu código aqui
+  document.querySelector("body").innerHTML += "<mark>Javascript ROCKS</mark>";
+});
+
+window.addEventListener("load", function () {
+  // escreva seu codigo aqui
+  document.querySelector("body").style.backgroundColor = "lightskyblue";
+  document.querySelector("h2").style.textAlign = "center";
+  document.querySelector("h4").style.fontStyle = "italic";
+});
+
+window.addEventListener("load", function () {
+  // escreva seu codigo aqui
+  let livros = document.querySelectorAll("li");
+
+  for (let livro of livros) {
+    // escreva seu codigo aqui
+    document.querySelector(livros).style.color = "orange";
+  }
+});
