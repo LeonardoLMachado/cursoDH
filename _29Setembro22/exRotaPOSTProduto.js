48845ffb26d19033f876c7ecb65ae0d64ec574fb
LeonardoLMachado <leonardolmachado@outlook.com.br>
2022-11-11 21:52:51 -0300

    29.09.22_34.10

---
+++ _29Setembro22/exRotaPOSTProduto.js
@@ -0,0 +1,11 @@
+let produto = {
+  tipoProduto: null,
+  preco: null,
+  quantidade: null,
+};
+
+produto.tipoProduto = "living";
+produto.preco = 1245;
+produto.quantidade = 300;
+
+app.post("/produto/criar", (req, res) => res.send(produto));
